f4d4be7db97b6002137db89ed5f2f952c5dab630
lzaquine <zaquinelucas@gmail.com>
2022-09-01 14:13:06 +0100

    commit

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -1,12 +1,38 @@
 import { Link } from 'react-router-dom';
+import fisherman from '../../Image/fisherman.png';
 
 
 function Apps() {
   return (
     <div>
       <h1>Apps</h1>
-      <Link to="/app/fisherman">fisherman</Link>
-      <Link to="/app/chat">Chat</Link>
+      <div className="ml-14 grid grid-rows-4 grid-cols-4">
+
+      <Link to="/app/fisherman">
+      <img src={fisherman} alt="logo" className='card w-24 absolute rounded-lg mt-20 saturate-200 glass'/>
+      </Link>
+      
+
+      <Link to="/app/chat">
+      <img src={fisherman} alt="logo" className='card w-24 absolute rounded-lg mt-20 saturate-200 glass'/> {/* ml-56 */}
+      </Link>
+
+      <Link to="/app/chat">
+      <img src={fisherman} alt="logo" className='card w-24 absolute rounded-lg mt-20 saturate-200 glass '/>
+      </Link>
+
+      <Link to="/app/chat">
+      <img src={fisherman} alt="logo" className='card w-24 absolute rounded-lg mt-20 saturate-200 glass '/>
+      </Link>
+
+      <Link to="/app/chat">
+      <img src={fisherman} alt="logo" className='card w-24 absolute rounded-lg mt-64 saturate-200 glass'/>
+      </Link>
+
+      <Link to="/app/chat">
+      <img src={fisherman} alt="logo" className='card w-24 absolute rounded-lg mt-64 saturate-200 glass '/>
+      </Link>
+      </div>
       <Link to="/">Home</Link>
       <section className="glass btn-wide w-full btn-sm btn-outline absolute bottom-0 text-white text-center opacity-70 h-24"><Link to="/">Teste</Link></section>
     </div>
