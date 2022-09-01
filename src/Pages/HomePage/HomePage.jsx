2e71b67d04d3f2f0f3693fc58d9f9c34fcca010c
lzaquine <zaquinelucas@gmail.com>
2022-09-01 15:23:01 +0100

    commit

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -23,7 +23,7 @@ function HomePage() {
         <li>Logos etc</li>
       </ul> */}
 
-      <video className="w-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" controls autoPlay >
+      <video className="w-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -mt-32" controls autoPlay >
         <source src={video} />
       </video>
 
