11438818b09c45b9fbd1d9755a0602d0f0d7fdb8
lzaquine <zaquinelucas@gmail.com>
2022-09-12 16:20:37 +0100

    Add BeatLoader Spinner and styles to it

--- src/Pages/EditProfile/EditProfile.jsx
+++ src/Pages/EditProfile/EditProfile.jsx
@@ -1,21 +1,24 @@
-import { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
-import { AuthContext } from '../../context/auth.context';
-import maispace  from "../../Image/maispace.png";
-
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context";
+import maispace from "../../Image/maispace.png";
+import BeatLoader from "react-spinners/BeatLoader";
 
 function EditProfile() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [field, setField] = useState('');
-  const { user }  = useContext(AuthContext); 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [field, setField] = useState("");
+  const { user } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
   const getProfile = async () => {
     try {
-      let response = await axios.get(`${process.env.REACT_APP_API_URL}/profile/${user._id}`);
+      let response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/profile/${user._id}`
+      );
 
       setName(response.data.name);
       setEmail(response.data.email);
@@ -26,6 +29,10 @@ function EditProfile() {
 
   useEffect(() => {
     getProfile();
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
   }, []);
 
   const handleName = (e) => setName(e.target.value);
@@ -40,61 +47,84 @@ function EditProfile() {
     axios
       .put(`${process.env.REACT_APP_API_URL}/editprofile/${user._id}`, body)
       .then(() => {
-        setName('');
-        setEmail('');
-        setField('');
+        setName("");
+        setEmail("");
+        setField("");
         navigate(`/profile`);
       })
       .catch((err) => console.log(err));
   };
 
-
   return (
     <div>
-     
-      <img src={maispace} alt="logo" className='w-24 absolute rounded-lg mt-24 saturate-200 glass p-2 absolute left-1/2 -translate-x-1/2 -translate-y-1/2'/>
+    {loading ? (
+        <BeatLoader
+          className="flex m-auto h-screen mt-60 justify-center items-center"
+          color={"#797a97"}
+          loading={loading}
+          size={20}
+        />
+      ) : (
+        <>
+
+      <img
+        src={maispace}
+        alt="logo"
+        className="w-24 absolute rounded-lg mt-24 saturate-200 glass p-2 absolute left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
       <div className="card w-96 saturate-200 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mb-20">
-      <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
+          <div class="collapse mt-10">
+            <input className="translate-x-1/3 mt-3" type="checkbox" />
+            <div class="collapse-title text-xl font-medium">
+              What's your name?
+            </div>
+            <div class="collapse-content">
+              <input
+                className="input input-bordered w-full rounded-full max-w-xs mt-1 mb-3 text-center text-neutral-400 opacity-95 justify-center"
+                type="text"
+                name="name"
+                placeholder={name}
+                onChange={handleName}
+              />
+            </div>
+          </div>
 
-      
-        <div class="collapse mt-10">
-        <input className="translate-x-1/3 mt-3" type="checkbox"/> 
-        <div class="collapse-title text-xl font-medium">What's your name?
-        {/* <label className="label label-text translate-x-1/4 text-xl" htmlFor="name">What's your name?</label> */}
-        </div>
-        <div class="collapse-content"> 
-        <input className="input input-bordered w-full rounded-full max-w-xs mt-1 mb-3 text-center text-neutral-400 opacity-95 justify-center" type="text" name="name" placeholder={name} onChange={handleName} />
+          <hr className="mt-2 mb-2 opacity-30" />
+
+          <div class="collapse">
+            <input className="translate-x-1/3 mt-3" type="checkbox" />
+            <div class="collapse-title text-xl font-medium">
+              What's your email?
+            </div>
+            <div class="collapse-content">
+              <input
+                className="input input-bordered w-full rounded-full max-w-xs mt-1 mb-3 text-center text-neutral-400 opacity-95 justify-center"
+                type="email"
+                name="email"
+                placeholder={email}
+                onChange={handleEmail}
+              />
+            </div>
+          </div>
+
+          <button
+            className="btn glass w-full btn-sm rounded-full text-white text-center mt-12 mb-2"
+            type="submit"
+          >
+            Edit PROFILE
+          </button>
+        </form>
+
+        <Link
+          to="/profile"
+          className="btn  btn-sm rounded-full text-white ml-19 text-center "
+        >
+          <b>BACK TO PROFILE</b>
+        </Link>
       </div>
-        </div>
-
-          <hr className="mt-2 mb-2 opacity-30"/>
-
-        <div class="collapse">
-        <input className="translate-x-1/3 mt-3" type="checkbox"/>
-        <div class="collapse-title text-xl font-medium">What's your email?
-       {/*  <label className="label label-text translate-x-1/4 text-xl" htmlFor="email">What's your email?</label> */}
-        </div>
-        <div class="collapse-content">
-        <input className="input input-bordered w-full rounded-full max-w-xs mt-1 mb-3 text-center text-neutral-400 opacity-95 justify-center" type="email" name="email" placeholder={email} onChange={handleEmail} />
-        </div>
-        </div>
-        
-        
-        {/* <label htmlFor="select"></label>
-        <select name="select" id="select" onChange={handleField}>Field:
-            <option placeholder={field}>{field}</option>
-            <option value="Fun">Fun</option>
-            <option value="Business">Business</option>
-            </select> */}
-
-        <button className="btn glass w-full btn-sm rounded-full text-white text-center mt-12 mb-2" type="submit">Edit PROFILE</button>
-      </form>
-      
-        
-        <Link to="/profile" className='btn  btn-sm rounded-full text-white ml-19 text-center '><b>BACK TO PROFILE</b></Link>
-      
-      </div>
-
+        </>
+        )}
     </div>
   );
 }
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,44 +2,71 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/auth.context";
 import video from "../../Image/video.MP4";
-import maispace  from "../../Image/maispace.png";
+import { useState, useEffect } from "react";
+import BeatLoader from "react-spinners/BeatLoader";
 
 function HomePage() {
   const { loggedIn } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
 
   return (
     <div>
-      <h1 className="p-5"> mAIspace </h1>
-
-      {/* <ul>
-        <li>T0D0 List:</li>
-        <li>fix the answers/questions & relate it to users (refresh problem).</li>
-        <li>Add field/profile pic to each user</li>
-        <li>Cloudinary</li>
-        <li>Fix the links/routes frontend as I wrote on my notebook</li>
-        <li>Footer</li>
-        <li>Styles</li>
-        <li>Find the dock</li>
-        <li>Logos etc</li>
-      </ul> */}
-
-      <video controls autoPlay className="w-1/2 absolute rounded-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -mt-32">
-        <source src={video} />
-      </video>
-      {/* <img src={maispace} alt="logo" className='w-24 absolute rounded-lg saturate-200 glass p-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '/> */}
-
+      {loading ? (
+        <BeatLoader
+          className="flex m-auto h-screen mt-60 justify-center items-center"
+          color={"#797a97"}
+          loading={loading}
+          size={20}
+        />
+      ) : (
+        <>
+          <video
+            controls
+            autoPlay
+            className="w-1/2 absolute rounded-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -mt-32"
+          >
+            <source src={video} />
+          </video>
+        </>
+      )}
 
       {loggedIn && (
         <>
-          <Link className="rounded-lg pl-8 pr-8 pt-2 pb-2 text-white ml-19 mt-52 glass text-center w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" to="/profile">PROFILE</Link>
-          <Link className="rounded-lg pl-8 pr-8 pt-2 pb-2 text-white ml-19 mt-64 glass text-center w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" to="/app">APPLICATIONS</Link>
-          {/* <Link to="/editprofile">Edit Profile</Link> */}
+          <Link
+            className="rounded-lg pl-8 pr-8 pt-2 pb-2 text-white ml-19 mt-52 glass text-center w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            to="/profile"
+          >
+            PROFILE
+          </Link>
+          <Link
+            className="rounded-lg pl-8 pr-8 pt-2 pb-2 text-white ml-19 mt-64 glass text-center w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            to="/app"
+          >
+            APPLICATIONS
+          </Link>
         </>
       )}
       {!loggedIn && (
         <>
-          <Link className="rounded-lg pl-8 pr-8 pt-2 pb-2 text-white ml-19 mt-52 glass text-center w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" to="/login">LOG IN</Link>
-          <Link className="rounded-lg pl-8 pr-8 pt-2 pb-2 text-white ml-19 mt-64 glass text-center w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" to="/signup">SIGN UP</Link>
+          <Link
+            className="rounded-lg pl-8 pr-8 pt-2 pb-2 text-white ml-19 mt-52 glass text-center w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            to="/login"
+          >
+            LOG IN
+          </Link>
+          <Link
+            className="rounded-lg pl-8 pr-8 pt-2 pb-2 text-white ml-19 mt-64 glass text-center w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            to="/signup"
+          >
+            SIGN UP
+          </Link>
         </>
       )}
     </div>
