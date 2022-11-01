6427d3d4ddaf5dc8500303aaeac71551fd907205
lzaquine <zaquinelucas@gmail.com>
2022-11-01 14:57:27 +0000

    Update toastify on english app

--- src/Pages/Apps/English.jsx
+++ src/Pages/Apps/English.jsx
@@ -105,7 +105,18 @@ function English() {
         >
           Submit
         </button>
-        <ToastContainer />
+        <ToastContainer
+position="top-center"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable={false}
+pauseOnHover
+theme="light"
+/>
       </form>
 
       <div>
