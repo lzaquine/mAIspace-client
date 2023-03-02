75003fe6e7341c7de659aba87c5b75a4145417b8
lzaquine <zaquinelucas@gmail.com>
2023-03-02 10:08:13 +0000

    test chat sort

--- src/Pages/Apps/Marv.jsx
+++ src/Pages/Apps/Marv.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext, useRef } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { AuthContext } from "../../context/auth.context";
 
@@ -7,9 +7,8 @@ function Marvbot() {
   const [question, setQuestion] = useState(null);
   const [answer, setAnswer] = useState("");
   const [results, setResults] = useState(null);
-  const [marvbotData, setMarvbotData] = useState(null);
+  const [app, setApp] = useState(null);
   const { user } = useContext(AuthContext);
-  const chatDivRef = useRef(null);
 
   const getMarvbot = async () => {
     try {
@@ -17,27 +16,21 @@ function Marvbot() {
         `${process.env.REACT_APP_API_URL}/app/marvbot`
       );
       if (response && response.data) {
-        // Extract the array of questions and answers
-        const marvbotData = response.data;
-        console.log(marvbotData)
-        const { updatedAt } = marvbotData;
-        console.log(marvbotData)
-        // Convert the string to an array
-        const sortedQa = JSON.parse(updatedAt);
-        console.log(sortedQa)
-        // Sort the array of questions and answers by date
-        sortedQa.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-  
-        // Set the sorted array of questions and answers to state
-        setMarvbotData({ ...marvbotData, updatedAt: sortedQa });
+        const sortedData = response.data.sort((a, b) =>
+          new Date(a.createdAt) - new Date(b.createdAt)
+        );
+        setResults(sortedData);
+        setQuestion(sortedData[0].question);
+        setPrompt(sortedData[0].prompt);
+        setAnswer(sortedData[0].answer);
+        setApp(sortedData[0]._id);
+        console.log(sortedData);
       }
+      
     } catch (error) {
       console.log(error);
     }
   };
-  
-  
-  
   
 
   const getUser = async () => {
@@ -55,12 +48,9 @@ function Marvbot() {
   
 
   useEffect(() => {
-    if (chatDivRef.current) {
-      chatDivRef.current.scrollTop = chatDivRef.current.scrollHeight;
-    }
     getMarvbot();
     getUser();
-  }, [results]);
+  }, [user]);
 
   /* const handleQuestion = (e) => setQuestion(e.target.value); */
   const token = localStorage.getItem("authToken");
@@ -106,28 +96,27 @@ function Marvbot() {
         </div>
       </form>
 
-      <div className="myChatDiv" ref={chatDivRef}>
-  {results &&
-    marvbotData &&
-    results
-      .filter((el) => el.app === marvbotData?.name)
-      .map((el, index) => {
-        return (
-          <div key={index} className="flex flex-col p-4 mb-2 chatDiv">
-            <div className="text-left font-bold flex-row mb-6">
-              <p className="text-[#423f3f]">You: {el.question}</p>
-            </div>
-            <div className="text-black">
-              <p>{el.answer}</p>
-            </div>
-            <div className="text-gray-500 text-sm text-right">
-              {new Date(el.createdAt).toLocaleString()}
-            </div>
+      <div className="myChatDiv">
+      {results &&
+  results
+    .filter((el) => el.app === app)
+    .map((el, index) => {
+      return (
+        <div key={index} className="flex flex-col p-4 mb-2 chatDiv">
+          <div className="text-left font-bold flex-row mb-6">
+            <p className="text-[#423f3f]">You: {el.question}</p>
           </div>
-        );
-      })}
-</div>
+          <div className="text-black">
+            <p>{el.answer}</p>
+          </div>
+          <div className="text-gray-500 text-sm text-right">
+            {new Date(el.createdAt).toLocaleString()}
+          </div>
+        </div>
+      );
+    })}
 
+      </div>
     </div>
   );
 }
