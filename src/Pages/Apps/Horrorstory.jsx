c6061c317ed9b5896036be4852dd134aa840200a
lzaquine <zaquinelucas@gmail.com>
2023-03-06 13:51:08 +0000

    Update HorrorStory

--- src/Pages/Apps/Horrorstory.jsx
+++ src/Pages/Apps/Horrorstory.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/auth.context";
-/* import screamnobg from "../../Image/screamnobg.png"; */
-import BeatLoader from "react-spinners/BeatLoader";
+import NavbarMarv from "../../components/NavbarMarv.jsx";
+{
+  /* STILL NEED TO UPDATE IT */
+}
 
 function Horrorstory() {
   const [prompt, setPrompt] = useState(null);
@@ -12,7 +13,6 @@ function Horrorstory() {
   const [results, setResults] = useState(null);
   const [app, setApp] = useState(null);
   const { user } = useContext(AuthContext);
-  const [loading, setLoading] = useState(false);
 
   const getHorrorstory = async () => {
     try {
@@ -31,10 +31,12 @@ function Horrorstory() {
 
   const getUser = async () => {
     try {
-      let response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/profile/${user._id}`
-      );
-      setResults(response.data.results);
+      if (user) {
+        let response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/profile/${user._id}`
+        );
+        setResults(response.data.results);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -43,13 +45,8 @@ function Horrorstory() {
   useEffect(() => {
     getHorrorstory();
     getUser();
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
   }, [user]);
 
-  const handleQuestion = (e) => setQuestion(e.target.value);
   const token = localStorage.getItem("authToken");
 
   const handleSubmit = (e) => {
@@ -73,70 +70,39 @@ function Horrorstory() {
 
   return (
     <div>
-      {/* <img
-        src={screamnobg}
-        alt="logo"
-        className="card w-24 rounded-lg left-1/2 transform -translate-x-1/2 mt-7 mb-3 saturate-200 glass justify-center"
-      /> */}
-
-      <h1>Horror Story</h1>
-
-      <h2 className="mt-2">
-        Creates two to three sentence short horror stories from a topic input.
-      </h2>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="question"
-          className="input input-bordered w-full rounded-lg max-w-xs mt-1 mb-3 mt-8 text-center text-neutral-400 opacity-95 justify-center"
-          placeholder="Word"
-          onChange={(e) => setQuestion(e.target.value)}
-          value={question}
-        />
-        <br />
-
-        <button
-          className="btn glass btn-sm rounded-full text-white text-center pl-5 pr-5 mb-8"
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
-
-      <div>
-        {loading ? (
-          <BeatLoader
-            className="flex m-auto h-screen mt-60 justify-center items-center"
-            color={"#797a97"}
-            loading={loading}
-            size={20}
-          />
-        ) : (
-          <div>
-            {results &&
-              results
-                .filter((el) => el.app === app)
-                .map((el, index) => {
-                  return (
-                    <p>
-                      <hr className="mb-2 mt-2 opacity-20" />
-                      {el.question}
-                      <br />
-
-                      {el.answer}
-                    </p>
-                  );
-                })}
+      <NavbarMarv />
+      <div className="myChatDiv">
+        {results &&
+          results
+            .filter((el) => el.app === app)
+            .map((el, index) => {
+              return (
+                <div key={index} className="flex flex-col p-4 mb-2 chatDiv">
+                  <div className="text-left font-bold flex-row mb-6">
+                    <p className="text-[#423f3f]">You: {el.question}</p>
+                  </div>
+                  <div className="text-black">
+                    <p>{el.answer}</p>
+                  </div>
+                </div>
+              );
+            })}
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <input
+              type="text"
+              name="question"
+              className="input input-bordered w-full rounded-lg max-w-xs mb-16 text-center justify-center"
+              placeholder="Who are you?"
+              onChange={(e) => setQuestion(e.target.value)}
+              value={question}
+            />
+            <button className="" type="submit">
+              Submit
+            </button>
           </div>
-        )}
+        </form>
       </div>
-      <Link
-        className="btn btn-sm rounded-full pl-8 pr-8 text-white ml-19 mt-10 text-center "
-        to="/app"
-      >
-        Back to Apps
-      </Link>
     </div>
   );
 }
