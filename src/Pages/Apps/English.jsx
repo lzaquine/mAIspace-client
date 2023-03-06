0b29b365a0b70846e794a3e3d27979b25ef41db3
lzaquine <zaquinelucas@gmail.com>
2023-03-06 13:51:01 +0000

    Update English

--- src/Pages/Apps/English.jsx
+++ src/Pages/Apps/English.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/auth.context";
-/* import english from "../../Image/english.jpeg"; */
 import BeatLoader from "react-spinners/BeatLoader";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import NavbarMarv from "../../components/NavbarMarv.jsx";
+{
+  /* STILL NEED TO UPDATE IT */
+}
 
 function English() {
   const [prompt, setPrompt] = useState(null);
@@ -15,19 +17,17 @@ function English() {
   const [app, setApp] = useState(null);
   const { user } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
-  const notify = () => toast.success('🦄 Wow so easy!', {
-    position: "top-center",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: false,
-    progress: undefined,
-    theme: "dark",
+  const notify = () =>
+    toast.success("🦄 Wow so easy!", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      progress: undefined,
+      theme: "dark",
     });
-
-  
-  
 
   const getEnglish = async () => {
     try {
@@ -46,10 +46,12 @@ function English() {
 
   const getUser = async () => {
     try {
-      let response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/profile/${user._id}`
-      );
-      setResults(response.data.results);
+      if (user) {
+        let response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/profile/${user._id}`
+        );
+        setResults(response.data.results);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -64,7 +66,6 @@ function English() {
     }, 1000);
   }, [user]);
 
-  const handleQuestion = (e) => setQuestion(e.target.value);
   const token = localStorage.getItem("authToken");
 
   const handleSubmit = (e) => {
@@ -88,78 +89,69 @@ function English() {
 
   return (
     <div>
-      {/* <img
-        src={english}
-        alt="logo"
-        className="card w-24 rounded-lg left-1/2 transform mt-7 mb-5 -translate-x-1/2 saturate-200 glass justify-center"
-      /> */}
-
-      <h1>Grammar correction</h1>
-
-      <h2 className="mt-2">Corrects sentences into standard English.</h2>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="question"
-          className="input input-bordered w-full rounded-lg max-w-xs mt-1 mb-3 mt-8 text-center text-neutral-400 opacity-95 justify-center"
-          placeholder="Me no good, me always hungry"
-          onChange={(e) => setQuestion(e.target.value)}
-          value={question}
-        />
-        <br />
-
-        <button
-          className="btn glass btn-sm rounded-full text-white text-center pl-5 pr-5 mb-8"
-          type="submit"
-          onClick={notify}
-        >
-          Submit
-        </button>
-        <ToastContainer
-position="top-center"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable={false}
-pauseOnHover
-theme="light"
-/>
-      </form>
+    <NavbarMarv />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        theme="light"
+      />
 
       <div>
-      {loading ? (
-        <BeatLoader className="flex m-auto h-screen mt-60 justify-center items-center" color={'#797a97'} loading={loading} size={20} />
-      ) : (
-        <div>
-        {results &&
-        results
-          .filter((el) => el.app === app)
-          .map((el, index) => {
-            return (
-              <p>
-                <hr className="mb-2 mt-2 opacity-20" />
-                {el.question}
-                <br />
-
-                {el.answer}
-              </p>
-            );
-          })}
+        {loading ? (
+          <BeatLoader
+            className="flex m-auto h-screen mt-60 justify-center items-center"
+            color={"#797a97"}
+            loading={loading}
+            size={20}
+          />
+        ) : (
+          <div>
+            <NavbarMarv />
+            <div className="myChatDiv">
+              {results &&
+                results
+                  .filter((el) => el.app === app)
+                  .map((el, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="flex flex-col p-4 mb-2 chatDiv"
+                      >
+                        <div className="text-left font-bold flex-row mb-6">
+                          <p className="text-[#423f3f]">You: {el.question}</p>
+                        </div>
+                        <div className="text-black">
+                          <p>{el.answer}</p>
+                        </div>
+                      </div>
+                    );
+                  })}
+              <form onSubmit={handleSubmit}>
+                <div className="input-container">
+                  <input
+                    type="text"
+                    name="question"
+                    className="input input-bordered w-full rounded-lg max-w-xs mb-16 text-center justify-center"
+                    placeholder="Who are you?"
+                    onChange={(e) => setQuestion(e.target.value)}
+                    value={question}
+                  />
+                  <button className="" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-      )}
+        )}
       </div>
-
-      
-      <Link
-        className="btn btn-sm rounded-full pl-8 pr-8 text-white ml-19 mt-10 text-center "
-        to="/app"
-      >
-        Back to Apps
-      </Link>
     </div>
   );
 }
